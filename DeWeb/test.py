7853219651cd045c9fec6c6f7364e5199b5901b3
DomeniGeco <domenigeco@gmail.com>
2026-03-13 16:57:03 +0100

    Update test.py

--- DeWeb/test.py
+++ DeWeb/test.py
@@ -1,5 +1,4 @@
 from DeWeb.browser import DeWebParser
 
-browser = DeWebParser(title="Pakrea - eswaiiOS")
-browser.load_url("https://demenigeco.github.io/functions/pk")
-browser.start()
+DeWebParser.prepare_load()
+DeWebParser.load("https://github.com/DeMENIGECO/DeWeb")
